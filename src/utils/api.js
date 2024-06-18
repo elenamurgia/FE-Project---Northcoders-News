304ccdb76644668464fd602203d481a9dc9d46b5
elenamurgia <145214693+elenamurgia@users.noreply.github.com>
2024-06-18 11:11:21 +0100

    creates a new file api.js where the axios has been extracted and it creates the base URL and amends the Articles.jsx and Topics.jsx files accordingly

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const articlesApi = axios.create({
+  baseURL: "https://nc-northcoders-news-api.onrender.com/api",
+});
+
+export const getArticles = () => {
+  return articlesApi.get("/articles").then((res) => {
+    return res.data;
+  });
+};
+
+export const getTopics = () => {
+  return articlesApi.get("/topics").then((res) => {
+    return res.data;
+  });
+};
