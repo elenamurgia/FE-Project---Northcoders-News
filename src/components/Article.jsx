451eb8e0bc252d9133c58f8022a110ae98dc03b9
elenamurgia <145214693+elenamurgia@users.noreply.github.com>
2024-06-21 09:19:08 +0100

    makes minor changes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,38 +1,29 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import {
-  getArticleById,
-  updateArticleVotes,
-  getArticlesByTopic,
-} from "../utils/api";
+import { getArticleById, updateArticleVotes } from "../utils/api";
 import Comments from "./Comments";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 
 function Article({ username }) {
-  const { article_id, topic } = useParams();
+  const { article_id } = useParams();
   const [article, setArticle] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [articleVotes, setArticleVotes] = useState(0);
 
   useEffect(() => {
-    setIs;
-    if (topic) {
-      getArticlesByTopic(topic).then((response) => {});
-    } else {
-      getArticleById(article_id)
-        .then(({ article }) => {
-          setArticle(article);
-          setArticleVotes(article.votes);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          setError("Something went wrong, please try again");
-          setIsLoading(false);
-        });
-    }
-  }, [article_id, topic]);
+    getArticleById(article_id)
+      .then(({ article }) => {
+        setArticle(article);
+        setArticleVotes(article.votes);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError("Something went wrong, please try again");
+        setIsLoading(false);
+      });
+  }, [article_id]);
 
   const handleVote = (vote) => {
     setArticleVotes((currentVotes) => currentVotes + vote);
