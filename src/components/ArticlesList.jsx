101899d06ee46adf1d6eebb3b690207f241f29f5
elenamurgia <145214693+elenamurgia@users.noreply.github.com>
2024-06-21 10:53:15 +0100

    makes changes to ArticlesList and Topics file to enable the user to view the articles associated with the selected topic

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,26 +1,42 @@
-import React, { useState, useEffect } from "react";
-import { getArticles } from "../utils/api.js";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getArticles, getArticlesByTopic } from "../utils/api.js";
+import { Row, Container, Col } from "react-bootstrap";
 import ArticleCard from "./ArticleCard.jsx";
 
 function ArticlesList() {
+  const { topic } = useParams();
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    getArticles()
-      .then(({ articles }) => {
-        setArticles(articles);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError("Something went wrong, please try again");
-        setIsLoading(false);
-      });
-  }, []);
+    if (topic) {
+      getArticlesByTopic(topic)
+        .then((articlesData) => {
+          if (articlesData.articles.length === 0) {
+            setError("No articles found for this topic");
+          } else {
+            setArticles(articlesData.articles);
+            setIsLoading(false);
+          }
+        })
+        .catch((error) => {
+          setError("Something went wrong, please try again");
+          setIsLoading(false);
+        });
+    } else {
+      getArticles()
+        .then((response) => {
+          setArticles(response.articles);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setError("Something went wrong, please try again");
+          setIsLoading(false);
+        });
+    }
+  }, [topic]);
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -32,7 +48,7 @@ function ArticlesList() {
 
   return (
     <Container>
-      <h2>Articles</h2>
+      <h2>Articles {topic && `- ${topic}`}</h2>
       <Row>
         {articles.map((article) => (
           <Col key={article.article_id} md={4} className="g-3">
