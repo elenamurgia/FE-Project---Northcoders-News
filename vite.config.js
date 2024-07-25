c5c7369175f9c73918a6f25b9c65e25af447beb0
elenamurgia <145214693+elenamurgia@users.noreply.github.com>
2024-07-25 14:41:21 +0100

    makes changes to vite.config.js to fix an issue with netlify

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-bootstrap-icons"],
+  resolve: {
+    alias: {
+      "react-bootstrap-icons": resolve(
+        __dirname,
+        "node_modules/react-bootstrap-icons"
+      ),
     },
   },
 });
